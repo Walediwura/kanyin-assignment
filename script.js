8dc415f1340762f53d8618fa6c0c99dd494bc8bf
Wale <82505372+Walediwura@users.noreply.github.com>
2023-03-26 22:52:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log('My name is Kanyinsola Cole. Javascript is my favourite language. In the the next one year I see myself getting comfortable building products that positively influence the world and exploring new technologies as the tech ecosystem changes and new technologies emerge.');
+console.log(` 1. My name is Kanyinsola Cole. 
+              2. Javascript is my favourite language. 
+              3. In the the next one year I see myself getting comfortable building products that positively influence the world and exploring new technologies as the tech ecosystem changes and new technologies emerge.`);
