eb21d79ad82a6f32916b5c2503a2ae86c329a61f
Wale <82505372+Walediwura@users.noreply.github.com>
2023-03-26 22:50:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+console.log('My name is Kanyinsola Cole. Javascript is my favourite language. In the the next one year I see myself getting comfortable building products that positively influence the world and exploring new technologies as the tech ecosystem changes and new technologies emerge.');
